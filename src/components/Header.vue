918eeae257baff207d241c681d56b87ec123a7af
Zalupaman <merkishev02@mail.ru>
2021-04-24 23:31:58 +0300

    Init and Auth UI

---
+++ src/components/Header.vue
@@ -0,0 +1,73 @@
+<template>
+  <header class="header">
+    <div class="container df">
+      <nav class="nav">
+        <p class="nav-title">Alex`s Forum</p>
+        <router-link to="/" class="nav-link linkHover">Home</router-link>
+        <router-link to="/" class="nav-link linkHover">Каталог</router-link>
+      </nav>
+      <div class="sing">
+        <router-link to="/auth/login">
+          <button class="sign-in">Log In</button>
+        </router-link>
+        <router-link to="/auth/signup">
+          <button class="sign-up">Sign Up</button>
+        </router-link>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+name: "Header"
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  width: 100%;
+  background: #ffffff;
+  padding: 15px;
+  box-sizing: border-box;
+}
+.df {
+  @include flex-between;
+}
+.nav {
+  display: flex;
+  align-items: center;
+  &-title {
+   font-size: 24px;
+    font-weight: 700;
+    padding-right: 10px;
+    margin-right: 10px;
+    color: $black;
+    border-right: 1px #27252540 solid;
+  }
+  &-link {
+    color: $black;
+    margin-right: 10px;
+    font-weight: 500;
+    font-size: 18px;
+  }
+}
+.sign {
+  &-in {
+    background: none;
+    color: $black;
+    border-radius: 6px;
+    padding: 5px 10px;
+    font-weight: 700;
+    box-sizing: border-box;
+  }
+  &-up {
+    background: $prim-background;
+    color: #ffffff;
+    border-radius: 6px;
+    padding: 5px 10px;
+    font-weight: 700;
+    box-sizing: border-box;
+  }
+}
+</style>
---
+++ src/views/Auth.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="auth-wrapper">
+    <div class="auth">
+      <p class="auth-title">{{ $route.params.type === 'login' ? 'Логин' : 'Регистрация' }}</p>
+      <form class="auth-content">
+        <input type="text" placeholder="Логин" class="auth-inp" v-model="login">
+        <input type="text" placeholder="Пароль" class="auth-inp" v-model="password">
+        <button class="auth-btn">Войти</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Auth",
+  data() {
+    return {
+      login: '',
+      password: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.auth {
+  background: #ffffff;
+  border-radius: 10px;
+  width: 300px;
+  padding: 25px;
+  &-wrapper{
+    width: 100%;
+    height: 91vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  &-content {
+    display: flex;
+    flex-direction: column;
+  }
+  &-title {
+    font-size: 24px;
+    color: $black;
+    margin-bottom: 15px;
+    margin-top: 0;
+    font-weight: bold;
+    text-align: center;
+  }
+  &-inp {
+    padding: 15px;
+    box-sizing: border-box;
+    outline: none;
+    margin-bottom: 10px;
+    border: none;
+    border-bottom: 1px $prim-background solid;
+  }
+  &-btn {
+    padding: 10px;
+    background: $prim-background;
+    border-radius: 6px;
+    font-size: 18px;
+    font-weight: 700;
+    color: #ffffff;
+  }
+}
+</style>
